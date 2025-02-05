6d51e8107749e5fed951c4f09fff6e5f2633f682
ChamaraSapumal <chamarasecu.21@gmail.com>
2025-02-05 08:09:29 +0530

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Enables static export
+  basePath: "/hi", // Replace with your GitHub repo name
+  assetPrefix: "/hi/", 
   images: {
+    unoptimized: true,
     domains: ["images.unsplash.com", "assets.aceternity.com"],
   },
 };
