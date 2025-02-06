5bf480f51a7a7cf555796c9c6990482292d3c356
ChamaraSapumal <chamarasecu.21@gmail.com>
2025-02-06 21:44:12 +0530

    third commit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enables static export for GitHub Pages
-  basePath: "/hi", // Change "hi" to your actual GitHub repo name
-  assetPrefix: "/hi/", // Must match basePath
+  basePath: "/portfolio", // Change "hi" to your actual GitHub repo name
+  assetPrefix: "/portfolio/", // Must match basePath
   images: {
     unoptimized: true, // Disable Next.js Image Optimization for static export
   },
